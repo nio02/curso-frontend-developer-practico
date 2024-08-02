d55f001672b06526591bf3426e8b856588139a41
NICO <annicolas2@gmail.com>
2024-08-01 22:08:05 -0500

    Mobile menu and Desktop Menu Included

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Event listener - desktop Menu
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
@@ -6,4 +7,15 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 function toggleDesktopMenu(){
     console.log('Click');
     desktopMenu.classList.toggle('inactive');
+}
+
+//Event Listener - Mobile Menu
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    console.log('Click');
+    mobileMenu.classList.toggle('inactive');
 }
