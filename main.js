00b1eb1dd1b98eda8c7d9c9be91942f6ff263733
NICO <annicolas2@gmail.com>
2024-08-03 12:42:03 -0500

    Menu Overlay problem solved with conditional functions

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
     console.log('Click');
+    //Conditional Shopping Cart Overlay Solution
+    const isAsideMenuClosed = shoppingCartMenu.classList.contains('inactive');
+
+    if (!isAsideMenuClosed){
+        //Si el mobile-menu está abierto, hay que cerrarlo
+        shoppingCartMenu.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
@@ -17,6 +25,14 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu(){
     console.log('Click');
+    //Conditional Shopping Cart Overlay Solution
+    const isAsideMenuClosed = shoppingCartMenu.classList.contains('inactive');
+
+    if (!isAsideMenuClosed){
+        //Si el mobile-menu está abierto, hay que cerrarlo
+        shoppingCartMenu.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
 }
 
@@ -26,7 +42,26 @@ const shoppingCartMenu = document.querySelector('.product-detail');
 
 menuShopCarIcon.addEventListener('click', toggleShoppingCartMenu);
 
+//---Conditional function solution
+
 function toggleShoppingCartMenu(){
-    console.log('Click');
+    //shoppingCartMenu.classList.toggle('inactive'); (Ya no simplemente incluiremos esto)
+    
+    //Mobile Menu Overlay
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed){
+        //Si el mobile-menu está abierto, hay que cerrarlo
+        mobileMenu.classList.add('inactive');
+    }
+
+    //Shopping Cart Menu Overlay
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isDesktopMenuClosed){
+        //Si el Desktop-menu está abierto, hay que cerrarlo
+        desktopMenu.classList.add('inactive');
+    }
+
     shoppingCartMenu.classList.toggle('inactive');
 }
